572e986515c928fe77c3d94c0ea488189e81d414
Uthaeus <Uth@github.com>
2017-12-08 20:18:15 -0700

    Generated blogs

---
+++ app/controllers/blogs_controller.rb
@@ -0,0 +1,16 @@
+class BlogsController < ApplicationController
+  def title:string
+  end
+
+  def author:string
+  end
+
+  def subject:string
+  end
+
+  def description:text
+  end
+
+  def thumb_image:text
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,14 @@
 Rails.application.routes.draw do
+  get 'blogs/title:string'
+
+  get 'blogs/author:string'
+
+  get 'blogs/subject:string'
+
+  get 'blogs/description:text'
+
+  get 'blogs/thumb_image:text'
+
   get 'pages/home'
 
   get 'pages/titles'
